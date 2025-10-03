78d8d3e5a62a685fbf8004d7e02ad8565119b012
Lucas Vinicius <l.vinicius1577@gmail.com>
2025-10-03 16:16:00 -0300

    change browserRouter to hashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import WWatch from "./pages/WWatch";
 import Movie from "./pages/Movie";
 import AllMovies from "./pages/AllMovies";
 import NotFound from "./pages/NotFound";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <WWatch />,
